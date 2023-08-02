f03a1184def092e3ad5bbc949a0984e856b26030
Aleksey-Tro <lescha.trofimow2016@yandex.ru>
2023-08-02 22:29:26 +0300

    task 2 is done

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,7 +1,23 @@
-﻿Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
+﻿//Console.WriteLine("Введите число");
+//int number = Convert.ToInt32(Console.ReadLine());
 
-int result = number * number;
+//int result = number * number;
 
-Console.WriteLine(result);
+//Console.WriteLine(result);
 
+
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 == number2*number2)
+{
+   Console.WriteLine("Да");
+}
+else
+{
+   Console.WriteLine("Нет");
+}
