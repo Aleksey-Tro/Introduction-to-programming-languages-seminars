7512acec2f75953c578010aa84b89cb0c76265b1
Aleksey-Tro <lescha.trofimow2016@yandex.ru>
2023-08-03 01:17:22 +0300

    Finished doing your homework

---
+++ Homework_1/Program.cs
@@ -0,0 +1,98 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите чило: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите чило: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1 < number2)
+// {
+
+//    Console.WriteLine(number2);
+// }
+// else if(number1 > number2)
+// {
+//    Console.WriteLine(number1);
+// }
+// else
+// {
+//    Console.WriteLine("Введите разные числа");
+// }
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите чило: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите чило: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите чило: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1 > number2)
+// {
+//    if(number1 > number3)
+//    {
+//       Console.WriteLine(number1);
+//    }
+//    else
+//    {
+//       Console.WriteLine(number3);
+//    }
+// }
+// else if(number2 > number3)
+// {
+//    Console.WriteLine(number2);
+// }
+// else
+// {
+//    Console.WriteLine(number3);
+// }
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Введите чило: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if((number % 2) == 0)
+// {
+//    Console.WriteLine("Число четное");
+// }
+// else
+// {
+//    Console.WriteLine("Число нечетное");
+// }
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Введите чило: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int i = 2;
+
+// while(i <= number)
+// {
+//    if((i % 2) ==0)
+//    {
+//       Console.WriteLine(i);
+//    }
+//    i++;
+// }
