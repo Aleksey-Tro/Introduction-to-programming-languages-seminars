5b9295a5139b515670e7cde612488430098406c8
Aleksey-Tro <lescha.trofimow2016@yandex.ru>
2023-08-02 23:35:12 +0300

    End of the seminar

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,4 +1,5 @@
-﻿//Console.WriteLine("Введите число");
+﻿
+//Console.WriteLine("Введите число");
 //int number = Convert.ToInt32(Console.ReadLine());
 
 //int result = number * number;
@@ -7,17 +8,79 @@
 
 
 
-Console.WriteLine("Введите число");
-int number1 = Convert.ToInt32(Console.ReadLine());
+//Задание 1
+// Console.WriteLine("Введите число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число");
-int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
 
-if(number1 == number2*number2)
-{
-   Console.WriteLine("Да");
-}
-else
-{
-   Console.WriteLine("Нет");
-}
+// if(number1 == number2*number2)
+// {
+//   Console.WriteLine("Да");
+// }
+// else
+// {
+//   Console.WriteLine("Нет");
+// }
+
+
+
+//Задание 3
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number == 1)
+// {
+//   Console.WriteLine("Понедельник");
+// }
+// else if(number == 2)
+// {
+//   Console.WriteLine("Вторник");
+// }
+// else if(number == 3)
+// {
+//   Console.WriteLine("Среда");
+// }
+// else if(number == 4)
+// {
+//   Console.WriteLine("Четверг");
+// }
+// else if(number == 5)
+// {
+//   Console.WriteLine("Пятница");
+// }
+// else if(number == 6)
+// {
+//   Console.WriteLine("Суббота");
+// }
+// else if(number == 7)
+// {
+//   Console.WriteLine("Восресенье");
+// }
+// else
+// {
+//   Console.WriteLine("Такого дня недели нет");
+// }
+
+
+// Задание 5
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int i = number * -1;
+
+// while(i <= number)
+// {
+//    Console.WriteLine(i);
+//    i++;
+// }
+
+
+// Задание 7
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int result = number % 10;
+
+// Console.WriteLine(result);
