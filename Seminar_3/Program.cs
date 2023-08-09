f447a4029c75b81b95990c8423e50c2090d13d4b
Aleksey-Tro <lescha.trofimow2016@yandex.ru>
2023-08-09 13:48:48 +0300

    saving

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача № 17. Напишите программу, которая
+// принимает на вход координаты точки (X и Y),
+// причем X и Y не равны 0 и выдает номер четверти
+// плоскости, в которой находится эта точка.
+
+// Console.WriteLine("Введите число ");
+// int X = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число ");
+// int Y = Convert.ToInt32(Console.ReadLine());
+
+// if(X > 0 && Y > 0)
+// {
+//    Console.WriteLine("1");
+// }
+// else if(X < 0 && Y > 0)
+// {
+//    Console.WriteLine("2");
+// }
+// else if(X < 0 && Y < 0)
+// {
+//    Console.WriteLine("3");
+// }
+// else if(X > 0 && Y < 0)
+// {
+//    Console.WriteLine("4");
+// }
+// else
+// {
+//    Console.WriteLine("Точка лежит на оси");
+// }
+
+
+// Задача 18.
+// Напишите программу, которая по заданному 
+// номеру четвертиПоказывает диапозон возможных
+// координат точек в этой четверти (X и Y).
+
+// Console.WriteLine("Введите число от 1 до 4");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number == 1)
+// {
+//    Console.WriteLine("X > 0, Y > 0");
+// }
+// else if(number == 2)
+// {
+//    Console.WriteLine("X < 0, Y > 0");
+// }
+// else if(number == 3)
+// {
+//    Console.WriteLine("X < 0, Y < 0");
+// }
+// else if(number == 4)
+// {
+//    Console.WriteLine("X > 0, Y < 0");
+// }
+// else
+// {
+//    Console.WriteLine("Введите номер от 1 до 4");
+// }
