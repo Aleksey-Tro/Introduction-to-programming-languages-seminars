a0f66a52bed3bd357e2942b5d4cae9071648e3ec
Aleksey-Tro <lescha.trofimow2016@yandex.ru>
2023-08-13 15:57:10 +0300

    commit

---
+++ Seminar_4/Program.cs
@@ -0,0 +1,66 @@
+﻿// // Задача 24: Напишите программу, которая принимает 
+// // на вход число (А) и выдает сумму чисел от 1 до А.
+// Console.WriteLine("Введите чило: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int Summ()
+// {
+//    int result = 0;
+//    for (int i = 1; i <= number; i++)
+//    {
+//       result = result + i;
+//    }
+//    return result;
+// }
+
+// Console.WriteLine(Summ());
+
+
+// Задача 26: Напишите программу, которая принимает на 
+// вход число и выдает количество цифр в числе.
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int N()
+// {
+//    int count = 0;
+//    if (number > 0)
+//    {
+//       while(number > 0)
+//       {
+//          number = number / 10;
+//          count++;
+//       }
+//    }
+//    else
+//    {
+//       while(number < 0)
+//       {
+//          number = number / 10;
+//          count++;
+//       }
+//    }
+//    return count;
+// }
+
+// Console.WriteLine(N());
+
+
+// Задача 28: Напишите программу, которая принимает на 
+// вход число N и выдает произведение чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Num()
+{
+   int result = 1;
+   for (int i = 1; i <= number; i++)
+   {
+      result = result * i;
+   }
+   return result;
+}
+
+int n = Num();
+Console.WriteLine(n);
